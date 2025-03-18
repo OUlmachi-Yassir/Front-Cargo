15d4d2b73ad89d61820be6d7208cfe1af2c203d0
OUlmachi-Yassir <oulmachi.yassir@gmail.com>
2025-03-18 12:18:23 +0000

    CG-22 costomise les layoutes

--- app/(client)/_layout.tsx
+++ app/(client)/_layout.tsx
@@ -1,26 +1,29 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
 import { Platform } from 'react-native';
-
-import { Colors } from './../../constants/Colors';
 import { useColorScheme } from '~/hooks/useColorScheme.web';
 import BlurTabBarBackground from '~/components/ui/TabBarBackground.ios';
-import { IconSymbol } from '~/components/ui/IconSymbol.ios';
 import { HapticTab } from '~/components/HapticTab';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
 
+  const orangeColors = {
+    active: '#F97316', 
+    inactive: '#FB923C',
+  };
+
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        tabBarActiveTintColor: orangeColors.active, 
+        tabBarInactiveTintColor: orangeColors.inactive, 
         headerShown: false,
         tabBarButton: HapticTab,
         tabBarBackground: BlurTabBarBackground,
         tabBarStyle: Platform.select({
           ios: {
-            // Use a transparent background on iOS to show the blur effect
             position: 'absolute',
           },
           default: {},
@@ -30,21 +33,39 @@ export default function TabLayout() {
         name="Clienthome"
         options={{
           title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={focused ? 'home' : 'home-outline'}
+              size={28}
+              color={color} 
+            />
+          ),
         }}
       />
       <Tabs.Screen
         name="Profile"
         options={{
           title: 'Profile',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={focused ? 'person' : 'person-outline'}
+              size={28}
+              color={color} 
+            />
+          ),
         }}
       />
       <Tabs.Screen
-        name="Notification"
+        name="ConversationsList"
         options={{
-          title: 'My Notification',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: 'Chat',
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={focused ? 'chatbubble' : 'chatbubble-outline'}
+              size={28}
+              color={color} 
+            />
+          ),
         }}
       />
     </Tabs>
